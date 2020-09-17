d1e1a7cd6ce1c1f94e8769e2d52b0c26d833118d
Sarah Allrozamo <sarahallrozamo@gmail.com>
2020-09-17 15:29:19 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+def my_collect(empty_array)
+  i=0
+  collection = []
+  while i < empty_array.length
+    #empty_array.upcase
+    collection << yield(empty_array[i])
+    i += 1
+    #empty_array.split(" ").first
+  end
+  collection
+end
+
 
 
